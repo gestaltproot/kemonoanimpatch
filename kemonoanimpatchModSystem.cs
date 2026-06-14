a0162f04e84d9c64bceae60f2c67215e5ceb558e
Gestalt <gestalt@gestalt.gay>
2026-06-14 00:36:42 -0600

    add patch to prevent crash stemming from kemono code

---
+++ kemonoanimpatchModSystem.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Linq;
+using HarmonyLib;
+using kemono;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using Vintagestory.API.MathTools;
+
+namespace kemonoanimpatch
+{
+    public class kemonoanimpatchModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            var harmony = new Harmony("kemonoanimpatch");
+            // Apply all Harmony patches in this assembly
+            harmony.PatchAll();
+
+            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("kemonoanimpatch:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("kemonoanimpatch:hello"));
+        }
+
+
+        public override void Dispose()
+        {
+            // Clean up any resources or patches when the mod is unloaded
+            var harmony = new Harmony("kemonoanimpatch");
+            harmony.UnpatchAll("kemonoanimpatch");
+        }
+
+
+
+    }
+    //[HarmonyPatch(typeof(kemono.KemonoPlayerHeadController))]
+    //[HarmonyPatch(MethodType.Constructor)]
+    //public class KemonoPlayerHeadControllerPatch
+    //{
+    //    public static bool Prefix(IAnimationManager animator, EntityPlayer entity, Shape entityShape, string head, string neck, ref string torsoUpper, ref string torsoLower, string footUpperL, string footUpperR, EnumAxis bodyForwardAxis, float bodyYawInterpolateThreshold, float bodyYawInterpolateSpeedBase, float bodyYawInterpolateSpeedDistMultiplier)
+    //    {
+    //        torsoUpper = "UpperTorso";
+    //        torsoLower = "LowerTorso";
+    //        return true;
+    //    }
+
+    //}
+
+    [HarmonyPatch(typeof(kemono.KemonoPlayerHeadController), "OnFrame")]
+    public class KemonoPlayerHeadControllerOnFramePatch
+    {
+        public static bool Prefix(kemono.KemonoPlayerHeadController __instance, float dt)
+        {
+            // Get necessary fields and properties via reflection to safely access them
+            var playerField = typeof(kemono.KemonoPlayerHeadController).GetField("player", BindingFlags.NonPublic | BindingFlags.Instance);
+            var entityPlayerField = typeof(kemono.KemonoPlayerHeadController).GetField("entityPlayer", BindingFlags.NonPublic | BindingFlags.Instance);
+            var entityField = typeof(kemono.KemonoPlayerHeadController).GetField("entity", BindingFlags.NonPublic | BindingFlags.Instance);
+            var upperTorsoPoseField = typeof(kemono.KemonoPlayerHeadController).GetField("UpperTorsoPose", BindingFlags.NonPublic | BindingFlags.Instance);
+            var lowerTorsoPoseField = typeof(kemono.KemonoPlayerHeadController).GetField("LowerTorsoPose", BindingFlags.NonPublic | BindingFlags.Instance);
+            var upperFootRPoseField = typeof(kemono.KemonoPlayerHeadController).GetField("UpperFootRPose", BindingFlags.NonPublic | BindingFlags.Instance);
+            var upperFootLPoseField = typeof(kemono.KemonoPlayerHeadController).GetField("UpperFootLPose", BindingFlags.NonPublic | BindingFlags.Instance);
+            var jointsHeadFoundProperty = typeof(kemono.KemonoPlayerHeadController).GetProperty("JointsHeadFound", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            var jointsBodyFoundProperty = typeof(kemono.KemonoPlayerHeadController).GetProperty("JointsBodyFound", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+            var entityPlayer = entityPlayerField?.GetValue(__instance);
+            var entity = entityField?.GetValue(__instance);
+            var player = playerField?.GetValue(__instance);
+
+            if (player == null && entityPlayer != null)
+            {
+                var playerProp = entityPlayer.GetType().GetProperty("Player");
+                player = playerProp?.GetValue(entityPlayer);
+                playerField?.SetValue(__instance, player);
+            }
+
+            var capi = (entity?.GetType().GetProperty("Api")?.GetValue(entity)) as ICoreClientAPI;
+
+            // FIXED: Instead of calling IsSelf(capi) which throws NullReferenceException, use true
+            bool isSelf = false;
+            bool isSelfImmersiveFp = false;
+
+            var upperTorsoPose = upperTorsoPoseField?.GetValue(__instance);
+            var lowerTorsoPose = lowerTorsoPoseField?.GetValue(__instance);
+            var upperFootRPose = upperFootRPoseField?.GetValue(__instance);
+            var upperFootLPose = upperFootLPoseField?.GetValue(__instance);
+
+            if (upperTorsoPose != null)
+            {
+                upperTorsoPose.GetType().GetProperty("degOffY")?.SetValue(upperTorsoPose, 0f);
+                upperTorsoPose.GetType().GetProperty("degOffZ")?.SetValue(upperTorsoPose, 0f);
+            }
+            if (lowerTorsoPose != null)
+            {
+                lowerTorsoPose.GetType().GetProperty("degOffZ")?.SetValue(lowerTorsoPose, 0f);
+            }
+            if (upperFootRPose != null)
+            {
+                upperFootRPose.GetType().GetProperty("degOffZ")?.SetValue(upperFootRPose, 0f);
+            }
+            if (upperFootLPose != null)
+            {
+                upperFootLPose.GetType().GetProperty("degOffZ")?.SetValue(upperFootLPose, 0f);
+            }
+
+            bool jointsHeadFound = (bool?)jointsHeadFoundProperty?.GetValue(__instance) ?? false;
+            bool jointsBodyFound = (bool?)jointsBodyFoundProperty?.GetValue(__instance) ?? false;
+
+            if (!isSelf)
+            {
+                if (jointsHeadFound)
+                {
+                    typeof(kemono.KemonoPlayerHeadController).GetMethod("AdjustHeadTilt", BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(__instance, new object[] { dt });
+                }
+                if (entity?.GetType().GetProperty("BodyYawServer")?.GetValue(entity) is float bodyYawServer && bodyYawServer == 0)
+                {
+                    entity.GetType().GetProperty("BodyYaw")?.SetValue(entity, entity.GetType().GetProperty("Pos")?.GetValue(entity)?.GetType().GetProperty("Yaw")?.GetValue(entity.GetType().GetProperty("Pos")?.GetValue(entity)));
+                }
+                return false;
+            }
+
+            if (capi?.Input.MouseGrabbed ?? false)
+            {
+                typeof(kemono.KemonoPlayerHeadController).GetMethod("AdjustAngles", BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(__instance, new object[] { capi, dt });
+            }
+
+            if (jointsHeadFound)
+            {
+                typeof(kemono.KemonoPlayerHeadController).GetMethod("AdjustHeadTilt", BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(__instance, new object[] { dt });
+            }
+
+            if (isSelfImmersiveFp && jointsBodyFound)
+            {
+                typeof(kemono.KemonoPlayerHeadController).GetMethod("AdjustTorsoTilt", BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(__instance, new object[] { dt });
+            }
+
+            return false;
+        }
+    }
+}
