4a621a3c2923babab12de41143d7b748e903188a
Gestalt <gestalt@gestalt.gay>
2026-06-14 00:36:04 -0600

    refining bone renaming, reorganize data sources

--- map_anims.py
+++ map_anims.py
@@ -1,9 +1,26 @@
 import json
 import sys
 
+bone_rename_map = {
+        "root": "b_Root",
+        # "LowerTorso": "b_TorsoLower",
+        # "UpperTorso": "b_TorsoUpper",
+        # "UpperArmR": "b_ArmUpperR",
+        # "UpperArmL": "b_ArmUpperL",
+        # "LowerArmL": "b_ArmLowerL",
+        # "LowerArmR": "b_ArmLowerR",
+        # "ItemAnchor": "b_ItemAnchor",
+        # "ItemAnchorL": "b_ItemAnchorL",
+        "Neck": "b_Neck",
+        "Head": "b_Head",
+        "UpperFootR": "b_FootUpperR",
+        "UpperFootL": "b_FootUpperL",
+        "LowerFootR": "b_FootLowerR",
+        "LowerFootL": "b_FootLowerL",
+    }
 
 def remap_rotation(rot_x, rot_y, rot_z):
-    return -rot_y, -rot_z, rot_x
+    return -rot_y, rot_z, rot_x
 
 def remap_position(pos_x, pos_y, pos_z):
     return -pos_y, -pos_z, pos_x
@@ -14,11 +31,8 @@ def process_keyframes(keyframes, ignore_anchor=True):
 
             for bone_name, bone in elements.items():
                 
-                ignore_list = ["b_Root", "b_TorsoLower", "b_TorsoUpper"]
-                if ignore_anchor:
-                    ignore_list.append("b_ItemAnchor")
-
-                if bone_name in ignore_list:
+                valid_bones = set(bone_rename_map.values())
+                if bone_name not in valid_bones:
                     continue
                 
                 rx = bone.get("rotationX")
@@ -61,130 +75,110 @@ def process_animation(data):
 
     for animation in animations:
 
-        # Manual overrides for specific animations IMPORTANT: ADD A CONTINUE AFTER EACH OVERRIDE
-        if animation.get("code") in ["snowshovel", "snowshovel-fp"]:
-            keyframes = [{
-					"frame": 0,
-					"elements": {
-						"b_ArmUpperR": {"rotationX": 63.0, "rotationY": -12.0, "rotationZ": 0.0 },
-						"b_ArmLowerR": {"rotationX": -136.0, "rotationY": 0.0, "rotationZ": 0.0 },
-
-						"b_ArmUpperL": { "rotationX": -45.0, "rotationY": -12.0, "rotationZ": 20.0 },
-						"b_ArmLowerL": { "rotationX": -20.0 , "rotationY": 0.0, "rotationZ": 0.0 },
-						"b_ItemAnchor": {"rotationX": 85.0, "rotationY": 18.0, "rotationZ": 0.0 },
-
-                        "b_TorsoLower": {"rotationX": -1.0, "rotationY": 0.0, "rotationZ": 2.0},
-                        "b_TorsoUpper": {"rotationX": 0.0, "rotationY": 3.0, "rotationZ": 17.0}
-					}
-				}]
-            
-            process_keyframes(keyframes, ignore_anchor=False)
-                
-            animation["keyframes"] = keyframes
-            # Forgot to add a continue here, but the animation looks fine as defined so we will not
-
         if animation.get("code") in ["wormgrunting", "wormgrunting-fp"]:  # Uses lots of leg changes, needs some offset adjustments
-            keyframes = [
-				{
-					"frame": 0,
-					"elements": {
-						"b_TorsoUpper": { "offsetX": 0.0, "offsetY": 0.0, "offsetZ": -0.5, "rotationX": -4.0, "rotationY": -14.0, "rotationZ": 8.0 },
-						"b_ArmUpperR": { "offsetX": 1.0, "offsetY": -0.2, "offsetZ": 0.2, "rotationX": 0.0, "rotationY":-7.0 , "rotationZ": -78.0 },
-						"b_ArmLowerR": { "offsetX": 0.0, "offsetY": 0.6, "offsetZ": 0.0, "rotationX": -32.0, "rotationY": 9.0, "rotationZ": -30.0 },
-						"b_ItemAnchor": { "offsetX": 1.2, "offsetY": 0.6, "offsetZ": -0.7, "rotationX": -16.0, "rotationY": 0.0, "rotationZ": 180.0 },
-						"b_ArmUpperL": { "offsetX": 0.4, "offsetY": 0.0, "offsetZ": -0.4, "rotationX": -88.0, "rotationY": -46.0, "rotationZ": -81.0 },
-						"b_ArmLowerL": { "offsetX": 0.1, "offsetY": 0.4, "offsetZ": 0.0, "rotationX": -15.0, "rotationY": -11.0, "rotationZ": -30.0 },
-						"b_Neck": { "rotationX": 0.0, "rotationY": 0.0, "rotationZ": 13.0 },
-						"b_Head": { "rotationX": 0.0, "rotationY": 0.0, "rotationZ": 13.0 },
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 16.0, "rotationZ": 15.0 }
-					}
-				},
-				{
-					"frame": 3,
-					"elements": {
-						"b_TorsoUpper": { "rotationX": -4.0, "rotationY": -15.0, "rotationZ": 14.0 },
-						"b_ArmUpperR": { "offsetX": 1.0, "offsetY": -0.2, "offsetZ": 0.2, "rotationX": 0.0, "rotationY":-7.0 , "rotationZ": -78.0 },
-						"b_ArmUpperL": { "offsetX": 0.4, "offsetY": -0.6, "offsetZ": -0.4, "rotationX": -88.0, "rotationY": -45.0, "rotationZ": -75.0 },
-						"b_ArmLowerL": { "rotationX": -14.0, "rotationY": -9.0, "rotationZ": -37.0 },
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 19.0, "rotationZ": 16.0 }
-					}
-				},
-				{
-					"frame": 4,
-					"elements": {
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 23.0, "rotationZ": 9.0 }
-					}
-				},
-				{
-					"frame": 5,
-					"elements": {
-						"b_TorsoUpper": { "offsetX": -0.2, "offsetY": 0.0, "offsetZ": -0.5 },
-						"b_ArmUpperR": { "offsetX": 1.0, "offsetY": -0.2, "offsetZ": 0.2, "rotationX": 0.0, "rotationY":-7.0 , "rotationZ": -78.0 },
-						"b_ArmUpperL": { "rotationX": -88.0, "rotationY": -46.1429, "rotationZ": -84.43 },
-						"b_ArmLowerL": { "rotationX": -14.2857, "rotationY": -9.5714, "rotationZ": -21.5714 },
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 20.0, "rotationZ": 12.0 }
-					}
-				},
-				{
-					"frame": 6,
-					"elements": {
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 21.0, "rotationZ": 6.0 }
-					}
-				},
-				{
-					"frame": 7,
-					"elements": {
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 14.0, "rotationZ": 8.0 }
-					}
-				},
-				{
-					"frame": 8,
-					"elements": {
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 16.0, "rotationZ": 3.5 }
-					}
-				},
-				{
-					"frame": 9,
-					"elements": {
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 11.0, "rotationZ": 7.0 }
-					}
-				},
-				{
-					"frame": 10,
-					"elements": {
-						"b_ArmUpperL": { "offsetX": 0.1, "offsetY": -1.6, "offsetZ": -0.4, "rotationX": -61.0, "rotationY": -48.0, "rotationZ": -83.0 },
-						"b_ArmLowerL": { "offsetX": 0.1, "offsetY": -0.2, "offsetZ": 0.0, "rotationX": -15.0, "rotationY": -11.0, "rotationZ": 3.0 },
-						"b_ItemAnchorL": { "rotationX": 0.0, "rotationY": 10.0, "rotationZ": 7.0 },
-						"b_TorsoUpper": { "offsetX": -0.1, "offsetY": 0.0, "offsetZ": -0.5, "rotationX": -4.0, "rotationY": -16.0, "rotationZ": 18.0 },
-						"b_ArmUpperR": { "offsetX": 1.0, "offsetY": -0.2, "offsetZ": 0.2, "rotationX": 0.0, "rotationY":-7.0 , "rotationZ": -78.0 },
-					}
-				},
-				{
-					"frame": 13,
-					"elements": {
-						"b_ArmUpperL": { "offsetX": 0.4, "offsetY": -1.2, "offsetZ": -0.4 }
-					}
-				},
-				{
-					"frame": 17,
-					"elements": {
-						"b_ArmUpperL": { "offsetX": 0.4, "offsetY": -0.2, "offsetZ": -0.4 }
-					}
-				}
-			]
-            process_keyframes(keyframes, ignore_anchor=True)
+            keyframes = animation.get("keyframes", [])
+            
+            # keyframes = [
+			# 	{
+			# 		"frame": 0,
+			# 		"elements": {
+			# 			"UpperTorso": { "offsetX": 0.0, "offsetY": 0.0, "offsetZ": -0.5, "rotationX": -4.0, "rotationY": -14.0, "rotationZ": 8.0 },
+			# 			"UpperArmR": { "offsetX": 1.0, "offsetY": -0.2, "offsetZ": 0.2, "rotationX": 0.0, "rotationY":-7.0 , "rotationZ": -78.0 },
+			# 			"LowerArmR": { "offsetX": 0.0, "offsetY": 0.6, "offsetZ": 0.0, "rotationX": -32.0, "rotationY": 9.0, "rotationZ": -30.0 },
+			# 			"ItemAnchor": { "offsetX": 1.2, "offsetY": 0.6, "offsetZ": -0.7, "rotationX": -16.0, "rotationY": 0.0, "rotationZ": 180.0 },
+			# 			"UpperArmL": { "offsetX": 0.4, "offsetY": 0.0, "offsetZ": -0.4, "rotationX": -88.0, "rotationY": -46.0, "rotationZ": -81.0 },
+			# 			"LowerArmL": { "offsetX": 0.1, "offsetY": 0.4, "offsetZ": 0.0, "rotationX": -15.0, "rotationY": -11.0, "rotationZ": -30.0 },
+			# 			"b_Neck": { "rotationX": 0.0, "rotationY": 0.0, "rotationZ": 13.0 },
+			# 			"b_Head": { "rotationX": 0.0, "rotationY": 0.0, "rotationZ": 13.0 },
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 16.0, "rotationZ": 15.0 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 3,
+			# 		"elements": {
+			# 			"UpperTorso": { "rotationX": -4.0, "rotationY": -15.0, "rotationZ": 14.0 },
+			# 			"UpperArmR": { "offsetX": 1.0, "offsetY": -0.2, "offsetZ": 0.2, "rotationX": 0.0, "rotationY":-7.0 , "rotationZ": -78.0 },
+			# 			"UpperArmL": { "offsetX": 0.4, "offsetY": -0.6, "offsetZ": -0.4, "rotationX": -88.0, "rotationY": -45.0, "rotationZ": -75.0 },
+			# 			"LowerArmL": { "rotationX": -14.0, "rotationY": -9.0, "rotationZ": -37.0 },
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 19.0, "rotationZ": 16.0 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 4,
+			# 		"elements": {
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 23.0, "rotationZ": 9.0 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 5,
+			# 		"elements": {
+			# 			"UpperTorso": { "offsetX": -0.2, "offsetY": 0.0, "offsetZ": -0.5 },
+			# 			"UpperArmR": { "offsetX": 1.0, "offsetY": -0.2, "offsetZ": 0.2, "rotationX": 0.0, "rotationY":-7.0 , "rotationZ": -78.0 },
+			# 			"UpperArmL": { "rotationX": -88.0, "rotationY": -46.1429, "rotationZ": -84.43 },
+			# 			"LowerArmL": { "rotationX": -14.2857, "rotationY": -9.5714, "rotationZ": -21.5714 },
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 20.0, "rotationZ": 12.0 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 6,
+			# 		"elements": {
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 21.0, "rotationZ": 6.0 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 7,
+			# 		"elements": {
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 14.0, "rotationZ": 8.0 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 8,
+			# 		"elements": {
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 16.0, "rotationZ": 3.5 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 9,
+			# 		"elements": {
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 11.0, "rotationZ": 7.0 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 10,
+			# 		"elements": {
+			# 			"UpperArmL": { "offsetX": 0.1, "offsetY": -1.6, "offsetZ": -0.4, "rotationX": -61.0, "rotationY": -48.0, "rotationZ": -83.0 },
+			# 			"LowerArmL": { "offsetX": 0.1, "offsetY": -0.2, "offsetZ": 0.0, "rotationX": -15.0, "rotationY": -11.0, "rotationZ": 3.0 },
+			# 			"ItemAnchorL": { "rotationX": 0.0, "rotationY": 10.0, "rotationZ": 7.0 },
+			# 			"UpperTorso": { "offsetX": -0.1, "offsetY": 0.0, "offsetZ": -0.5, "rotationX": -4.0, "rotationY": -16.0, "rotationZ": 18.0 },
+			# 			"UpperArmR": { "offsetX": 1.0, "offsetY": -0.2, "offsetZ": 0.2, "rotationX": 0.0, "rotationY":-7.0 , "rotationZ": -78.0 },
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 13,
+			# 		"elements": {
+			# 			"UpperArmL": { "offsetX": 0.4, "offsetY": -1.2, "offsetZ": -0.4 }
+			# 		}
+			# 	},
+			# 	{
+			# 		"frame": 17,
+			# 		"elements": {
+			# 			"UpperArmL": { "offsetX": 0.4, "offsetY": -0.2, "offsetZ": -0.4 }
+			# 		}
+			# 	}
+			# ]
+            # process_keyframes(keyframes, ignore_anchor=True)
 
             keyframes[0]["elements"].update(
                 {
-                    "b_TorsoLower": {
+                    "LowerTorso": {
                             "rotShortestDistanceX": True,
                             "rotShortestDistanceY": True,
                             "rotShortestDistanceZ": True,
                             "rotationX": 0.0,
                             "rotationY": 0.0,
-                            "rotationZ": 24.999995,
-                            "offsetX": -6.31763124,
-                            "offsetY": -2.69455051,
+                            "rotationZ": 0.999995,
+                            "offsetY": -6.31763124,
+                            "offsetX": 2.69455051,
                             "offsetZ": -0.0
                         },
                         "b_FootUpperR": {
@@ -194,9 +188,6 @@ def process_animation(data):
                             "rotationX": 5.44816656,
                             "rotationY": 25.30363456,
                             "rotationZ": -4.51232648,
-                            "offsetX": -1.51281369,
-                            "offsetY": 0.27477747,
-                            "offsetZ": -1.62354302
                         },
                         "b_FootLowerR": {
                             "rotShortestDistanceX": True,
@@ -214,7 +205,7 @@ def process_animation(data):
                             "rotShortestDistanceY": True,
                             "rotShortestDistanceZ": True,
                             "rotationX": -5.44816442,
-                            "rotationY": 89.30358959,
+                            "rotationY": 0.30358959,
                             "rotationZ": 4.51232264,
                             "offsetX": -1.99365366,
                             "offsetY": -0.15893261,
@@ -283,23 +274,7 @@ def rename_bones(data):
     """
     Remaps bone names to the kemono format
     """
-    bone_rename_map = {
-        "root": "b_Root",
-        "LowerTorso": "b_TorsoLower",
-        "UpperTorso": "b_TorsoUpper",
-        "UpperArmR": "b_ArmUpperR",
-        "UpperArmL": "b_ArmUpperL",
-        "LowerArmL": "b_ArmLowerL",
-        "LowerArmR": "b_ArmLowerR",
-        "ItemAnchor": "b_ItemAnchor",
-        "ItemAnchorL": "b_ItemAnchorL",
-        "Neck": "b_Neck",
-        "Head": "b_Head",
-        "UpperFootR": "b_FootUpperR",
-        "UpperFootL": "b_FootUpperL",
-        "LowerFootR": "b_FootLowerR",
-        "LowerFootL": "b_FootLowerL",
-    }
+    
     
     # Convert data to JSON string
     json_str = json.dumps(data)
